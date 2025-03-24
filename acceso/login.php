ad2105aa10147c2c0cacd1051d7b629995587add
rubpedcar <rpedcar0608@g.educaand.es>
2025-03-24 15:48:13 +0100

    Estructura index y login

---
+++ acceso/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesión</title>
+</head>
+<body>
+    <h1>Iniciar Sesión</h1>
+
+    <form action="">
+        <p><input type="text" name="email" placeholder="Email"></p>
+        <p><input type="password" name="password" placeholder="Contraseña"></p>
+
+        <button type="submit">Continuar</button>
+    </form>
+
+    <br><a href="registro.php">¿Quieres participar? Regístrate aquí.</a>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,9 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Rally</title>
 </head>
 <body>
-    <h1>Fotos</h1>
+    <nav><a href="acceso/login.php">Iniciar Sesión</a></nav>
+
+    <h1>Vista Usuario</h1>
+    <h1>Rally Fotográfico</h1>
+
+    <div>
+        <p>Foto 1</p>
+        <p>Por participante1</p>
+        <p>Descripción</p>
+        <button>Votar</button>
+    </div>
+
+    <div>
+        <p>Foto 2</p>
+        <p>Por participante2</p>
+        <p>Descripción</p>
+        <button>Votar</button>
+    </div>
+
 </body>
 </html>
