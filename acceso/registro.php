44bca8619d621bea18be50c5ed7bb140e8e520de
rubpedcar <rpedcar0608@g.educaand.es>
2025-03-24 16:51:38 +0100

    Estructura registro

---
+++ acceso/registro.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+</head>
+<body>
+    <h1>Formulario de inscripción</h1>
+
+    <form action="">
+        <p>Nombre: <input type="text" name="nombre"></p>
+        <p>Email: <input type="text" name="email"></p>
+        <p>Contraseña: <input type="password" name="password"></p>
+        <p>Elige una foto de perfil:</p>
+
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
