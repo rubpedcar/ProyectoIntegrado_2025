b7d97b54e4aa02f4b27bed401588f3efc5886cea
rubpedcar <rpedcar0608@g.educaand.es>
2025-06-03 23:46:27 +0200

    Actualizar funcional

--- js/perfil.js
+++ js/perfil.js
@@ -4,8 +4,70 @@ document.addEventListener("DOMContentLoaded", function(event){
     document.getElementById("formEmail").value = localStorage.getItem("email");
 });
 
-document.getElementById("form").addEventListener("click", function(event){
+
+function validarDatos()
+{
+    let mensaje = document.getElementById("mensaje");
+    let nombre = document.getElementById("formNombre").value;
+    let email = document.getElementById("formEmail").value;
+
+    let res = true;
+
+    if(nombre == "" || email == "")
+    {
+        mensaje.innerText = "Debes rellenar todos los campos.";
+        res = false;
+    }
+    else
+    {
+        mensaje.innerText = "";
+    }
+
+    return res;
+}
+
+
+function enviarDatos()
+{
+    let id = this.localStorage.getItem("id");
+    let nombre = document.getElementById("formNombre").value;
+    let email = document.getElementById("formEmail").value;
+
+
+    const datos = {id, nombre, email};
+    //console.log(datos);
+
+    fetch("http://localhost/hlc/tarde/Backend_ProyectoIntegrado/usuarios.php", {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(datos)
+    })
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById("mensaje").textContent = data.mensaje;
+        
+        alert(data.mensaje);
+
+        window.location.href = "index.html";
+    })
+    .catch(err => {
+        console.log(err);
+        document.getElementById("mensaje").textContent = data.mensaje;
+    });
+}
+
+
+
+document.getElementById("form").addEventListener("submit", function(event){
     event.preventDefault();
+
+    if(validarDatos())
+    {
+        enviarDatos();
+    }
 
     
 });
+
